c3150d351a661488504a9977a6bf7118786871b0
Felipe Giraldo <juinfe2030@gmail.com>
2025-10-29 07:38:42 -0500

    Products List

--- ShopCauchosMC/src/index.js
+++ ShopCauchosMC/src/index.js
@@ -24,3 +24,34 @@ app.listen(PORT);
 console.log('Server on port', PORT)
 
 RabbitMQ.connect();
+//ShopCauchosMC%*07
+//docker-compose logs -f backend
+
+//dir
+//docker-compose build frontend
+//docker-compose up -d
+
+//GIT
+/*
+git add . && \
+rm -rf cauchosmc/.git && \
+git rm --cached -rf cauchosmc && \
+rm -rf ShopCauchosMC/.git && \
+git rm --cached -rf ShopCauchosMC && \
+git add cauchosmc && \
+git add ShopCauchosMC 
+git commit -m "deploy physical rentals"
+git push -u origin main
+
+git remote set-url origin https://github.com/usuario/repositorio2.git
+git push -u origin main*/
+
+/*
+import { encrypt, compare } from "./services/handleBcrypt.js";
+let hs = await encrypt("ShopCauchosMC%*07")
+console.log(hs)
+
+let kd = await compare("ShopCauchosMC%*07", hs)
+console.log(kd)*/
+
+// Test1 : APP_USR-6878764603009760-080420-5f8e85ae4413186528ad4e80e9a81c52-2606461440
